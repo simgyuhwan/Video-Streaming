d802a0cd9ee6b5ae4493a432fd20dd5a36cbc0b3
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-08-24 21:02:41 +0900

    history 서비스 추가

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,56 +1,78 @@
 const express = require("express");
+const fs = require("fs");
 const http = require("http");
 
-const app = express();
+function sendViewedMessage(videoPath) {
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  };
 
-//
-// Throws an error if the any required environment variables are missing.
-//
+  const requestBody = {
+    videoPath: videoPath
+  };
 
-if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  const req = http.request(
+    "http://history/viewed",
+    postOptions
+  );
+
+  req.on("close", () => {
+    console.log("Sent 'viewed' message to history microservice");
+  });
+
+  req.on("error", (err) => {
+    console.log("Failed to send 'viewed' message!");
+    console.log(err && err.stack || err);
+  });
+
+  req.write(JSON.stringify(requestBody));
+  req.end();
 }
 
-if (!process.env.VIDEO_STORAGE_HOST) {
-    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+function setupHandlers(app) {
+  app.get("/video", (req, res) => {
+    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(videoPath, (err, stats)=> {
+      if(err) {
+        console.error("An error occurred ");
+        res.sendStatus(500);
+        return;
+      }
+
+      res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4",
+      });
+
+      fs.createReadStream(videoPath).pipe(res);
+
+      sendViewedMessage(videoPath);
+    });
+  });
 }
 
-if (!process.env.VIDEO_STORAGE_PORT) {
-    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+function startHttpServer() {
+  return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
+      const app = express();
+      setupHandlers(app);
+      
+      const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
+      app.listen(port, () => {
+          resolve();
+      });
+  });
 }
 
-//
-// Extracts environment variables to globals for convenience.
-//
-const PORT = process.env.PORT;
-const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
-const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
-console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
+function main() {
+  return startHttpServer();
+}
 
-//
-// Registers a HTTP GET route for video streaming.
-//
-app.get("/video", (req, res) => {
-    const forwardRequest = http.request( // Forward the request to the video storage microservice.
-        {
-            host: VIDEO_STORAGE_HOST,
-            port: VIDEO_STORAGE_PORT,
-            path: '/video?path=SampleVideo_1280x720_1mb.mp4', // Video path is hard-coded for the moment.
-            method: 'GET',
-            headers: req.headers
-        }, 
-        forwardResponse => {
-            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-            forwardResponse.pipe(res);
-        }
-    );
-    
-    req.pipe(forwardRequest);
-});
-
-//
-// Starts the HTTP server.
-//
-app.listen(PORT, () => {
-    console.log(`Microservice online`);
-});
+main()
+  .then(() => console.log("Microservice online."))
+  .catch(err => {
+      console.error("Microservice failed to start.");
+      console.error(err && err.stack || err);
+  });
